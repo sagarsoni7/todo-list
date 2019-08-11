40bb3f5ee263c8d945a5b76959aabe08953103e2
Sagar Soni <sagar.soni@zoho.com>
2019-08-12 01:16:47 +0530

    added addition and deletion handler

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,20 @@
+const bodyParser=require("body-parser");    
+const urlencodedParser=bodyParser.urlencoded({extended: false});
 let data=[{item: "get banana"},{item: "go bath"},{item: "go to college"}];
 
+
 module.exports=(app)=>{
-    app.get("/todo",(req,res)=>{
+    app.get("/todo", (req,res)=>{
         res.status(200).render("todo",{todos: data});
     });
-    app.post("/todo",(req,res)=>{
-
+    app.post("/todo",urlencodedParser,(req,res)=>{
+        data.push(req.body);
+        res.json(data);
     });
-    app.delete("/todo",(req,res)=>{
-
+    app.delete("/todo/:item",(req,res)=>{
+        data=data.filter((todo)=>{
+            return todo.item.replace(/ /g,"-") !== req.params.item;
+        });
+        res.json(data);
     })
 }
